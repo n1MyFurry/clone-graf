a349010a2c5658e1a3b49c022231201f037edbbd
n1MyFurry <k.github@yandex.ru>
2023-07-30 23:27:15 +0300

    another15

--- app/page.tsx
+++ app/page.tsx
@@ -41,31 +41,33 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
 
     if(projectsToDisplay.length === 0) {
 
-        const data = await fetchAllProjectsWithoutCategory() as ProjectSearch;
+        // const data = await fetchAllProjectsWithoutCategory() as ProjectSearch;
 
-        const prToDisp = data?.projectSearch?.edges || [];
+        // const prToDisp = data?.projectSearch?.edges || [];
 
 
-        if (prToDisp.length !== 0) {
-            return (
-                <section className="flex-start flex-col paddings mb-16">
-                    <Categories />
-                    <section className="projects-grid">
-                        {prToDisp.map(({ node }: { node: ProjectInterface }) => (
-                            <ProjectCard 
-                                key={node?.id}
-                                id={node?.id}
-                                image={node?.image}
-                                title={node?.title}
-                                name={node?.createdBy?.name}
-                                avatarUrl={node?.createdBy?.avatarUrl}
-                                userId={node?.createdBy?.id}
-                            />
-                        ))}
-                    </section>
-                </section>
-            );
-        } else {
+        // if (prToDisp.length !== 0) {
+        //     return (
+        //         <section className="flex-start flex-col paddings mb-16">
+        //             <Categories />
+        //             <section className="projects-grid">
+        //                 {prToDisp.map(({ node }: { node: ProjectInterface }) => (
+        //                     <ProjectCard 
+        //                         key={node?.id}
+        //                         id={node?.id}
+        //                         image={node?.image}
+        //                         title={node?.title}
+        //                         name={node?.createdBy?.name}
+        //                         avatarUrl={node?.createdBy?.avatarUrl}
+        //                         userId={node?.createdBy?.id}
+        //                     />
+        //                 ))}
+        //             </section>
+        //         </section>
+        //     );
+        // } else {
+            // }
+            
             return (
                 <section className="flexStart flex-col paddings">
                     <Categories />
@@ -73,8 +75,6 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
                 </section>
             );
         }
-
-    }
 
     return (
         <section className="flex-start flex-col paddings mb-16">
